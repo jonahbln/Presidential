bdf1ae32014888b12abd3878fd1b6a3b4bbd1555
Still-Searching727 <73491181+Still-Searching727@users.noreply.github.com>
2023-04-17 16:47:56 -0400

    President animation

---
+++ Game/Assets/Scripts/PresidentBehavior.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PresidentBehavior : MonoBehaviour
+{
+
+    public static bool transformation = false;
+    bool calledAnim = false;
+    Animator anim;
+    bool appQuit;
+
+    public GameObject boss;
+    public GameObject explosion;
+
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+        transformation = false;
+        calledAnim = false;
+        appQuit = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transformation && !calledAnim)
+        {
+            Debug.Log("LOL");
+            anim.SetTrigger("Transformation");
+            calledAnim = true;
+            Destroy(gameObject, 4.5f);
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        appQuit = true;
+    }
+
+    private void OnDestroy()
+    {
+        if (appQuit) return;
+        Instantiate(boss, transform.position, transform.rotation);
+        GameObject g = Instantiate(explosion, transform.position, transform.rotation);
+        Destroy(g, 2f);
+    }
+
+}
